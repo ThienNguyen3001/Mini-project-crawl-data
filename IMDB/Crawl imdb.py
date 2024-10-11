87c922aa2d0533088aa652229a86e8c05c06a4f2
Thien Nguyen <thiennguyen03001@gmail.com>
2024-10-11 22:36:23 +0700

    first commit

---
+++ IMDB/Crawl imdb.py
@@ -0,0 +1,56 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+import pandas as pd
+
+# Đường dẫn đến ChromeDriver
+
+driver = webdriver.Chrome()
+
+# Mở trang IMDb top chart
+url = "https://www.imdb.com/chart/top/"
+driver.get(url)
+
+# Khởi tạo các danh sách để lưu trữ dữ liệu
+titles = []
+publish_years = []
+times = []
+movie_types = []
+ratings = []
+vote_counts = []
+
+# Tìm tất cả các mục phim
+movie_repo = driver.find_elements(By.CLASS_NAME, 'ipc-metadata-list-summary-item__tc')
+
+for movie in movie_repo:
+    title = movie.find_element(By.CLASS_NAME, 'ipc-title__text').text.strip()
+    metadata_items = movie.find_elements(By.CLASS_NAME, 'sc-ab348ad5-8.cSWcJI.cli-title-metadata-item')
+    publish_year = metadata_items[0].text.strip() if len(metadata_items) > 0 else None
+    time = metadata_items[1].text.strip() if len(metadata_items) > 1 else None
+    movie_type = metadata_items[2].text.strip() if len(metadata_items) > 2 else None
+    rating = movie.find_element(By.CLASS_NAME, 'ipc-rating-star--rating').text.strip()
+    vote_count = movie.find_element(By.CLASS_NAME, 'ipc-rating-star--voteCount').text.strip()
+
+    # Thêm dữ liệu vào các danh sách
+    titles.append(title)
+    publish_years.append(publish_year)
+    times.append(time)
+    movie_types.append(movie_type)
+    ratings.append(rating)
+    vote_counts.append(vote_count)
+
+# Đóng trình duyệt sau khi crawl xong
+driver.quit()
+
+# Tạo DataFrame từ các danh sách
+df = pd.DataFrame({
+    "Title": titles,
+    "Publish year": publish_years,
+    "Time": times,
+    "Movie type": movie_types,
+    "Rating": ratings,
+    "Vote count": vote_counts
+}).to_csv("IMDB\IMDB_top_chart.csv", index=False)
+
+# Hiển thị DataFrame
+print(df)
